bc92c1fe87bd14364599ac65453bb40b35d1e96b
Leonidas <britos659@gmail.com>
2023-08-17 21:53:06 -0300

    corección de errores

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,18 @@ import express from "express";
 
 let router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with all users');
-});
+//router.post('/',funcion)
+//router.metodo('/',funcion)
+//metodo http que: para crear es POST, para leer es GET, para actualizar es PUT/PATCH y para eliminar es DELETE
+//funcion que se va a ejecutar UNA UNICA VEZ cada vez que se llame al endpoint de manera que
+//cada vez que realizo una petición POST, se creará un recurso
+//cada vez que realizo una petición GET, se leeran recursos
+//cada vez que realizo una peticion PUT/PATCH, se actualizaran un recurso
+//cada vez que realizo una peticion DELETE, se eleiminara un recurso
+
+//CREATE
+router.post('/'. function (req, res)=> {
+  
+})
 
 export default router;
