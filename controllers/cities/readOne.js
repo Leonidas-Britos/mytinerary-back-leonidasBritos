a11970182bc4cd075495b88935f5a646f0db1807
Leonidas <britos659@gmail.com>
2023-08-28 11:58:38 -0300

    comienzo del crud

--- controllers/cities/readOne.js
+++ controllers/cities/readOne.js
@@ -1,24 +1,22 @@
 import City from "../../models/City.js";
 
-export default async (req, res, next) => {
-  try {
-    let one = await City.findOne({ _id: req.params.city_id }).select(
-      "country city -_id"
-    );
-    if (one) {
-      return res.status(200).json({
-        success: true,
-        message: "City found",
-        response: one
-      });
-    } else {
-      return res.status(404).json({
-        success: false,
-        message: "Not found city",
-        response: null
-      });
+export default async (req,res,next) => {
+    try {
+        let one = await City.findById(req.params.city_id)
+        if (one) {
+            return res.status(200).json({
+                success: true,
+                message: 'city found',
+                response: one
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: 'not found',
+                response: null
+            })
+        }
+    } catch (error) {
+        next(error)
     }
-  } catch (error) {
-    next(error);
-  }
-};
+}
--- models/Itinerary.js
+++ models/Itinerary.js
@@ -1,14 +1,16 @@
 import { model, Schema, Types } from "mongoose";
 
-let collection = 'itineraries';
+let collection = "itineraries"
 let schema = new Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    duration: { type: String, required: true },
-    tags: { type: [String] },
-    photo: { type: String, default: "https://www.cinemascomics.com/wp-content/uploads/2020/06/poder-darth-vader.jpg" },
-    city_id: { type: Types.ObjectId, required: true, ref: 'cities' }
-});
+    name: { type:String,required:true },
+    city_id: { type:Types.ObjectId,ref:'cities',required:true },
+    price: { type:Number,required:true },
+    duration: { type:Number,required:true },
+    tags: [{ type:String,required:true }],
+    photo: { type:String,required:true }
+},{
+    timestamps: true    //agrega dos propiedades de tiempo (fecha de creación y fecha de ultima modificación)
+})
 
 let Itinerary = model(collection, schema);
 export default Itinerary;
