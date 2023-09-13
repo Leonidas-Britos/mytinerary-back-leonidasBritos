9d0c3544954de9ac4e92756d893d6d37d68e32ae
Leonidas <britos659@gmail.com>
2023-09-13 01:01:31 -0300

    fixes

--- controllers/activities/read.js
+++ controllers/activities/read.js
@@ -2,21 +2,17 @@ import Activity from "../../models/Activity.js";
 
 export default async (req, res, next) => {
   try {
-    let allActivities = await Activity.find();
-    if (allActivities.length > 0) {
-      return res.status(200).json({
-        success: true,
-        message: "Activities found",
-        response: allActivities,
-      });
-    } else {
-      return res.status(404).json({
-        success: false,
-        message: "Not found",
-        response: null,
-      });
+    let queries = {}
+    if (req.query.itinerary_id) {
+      queries.itinerary_id = req.query.itinerary_id
     }
+    let allActivities = await Activity.find(queries)
+    return res.status(200).json({
+      success: true,
+      message: 'activities found',
+      response: allActivities
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
